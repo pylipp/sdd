b2dde8d722099650e1e3678f4f1350bc98b137ec
pylipp <philipp.metzner@tum.de>
2017-09-28 17:17:17 +0200

    Generalize st installation and move to separate script.

---
+++ install_st.bash
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# https://st.suckless.org/ simple terminal
+
+# toggle light/dark theme by F6
+# zoom in/out via Ctrl-Shift-PageUp/Down
+# copy/paste via Ctrl-Shift-C/V
+
+source "$HOME"/.files/setup/utils.bash
+
+echo_install_info simple-terminal
+mkcswdir
+
+install_packages libxft-dev
+
+# clone source and checkout revision that matches the patches
+git clone git://git.suckless.org/st
+cd st || ( echo "Error cloning..."; exit )
+git checkout b331da5
+
+# apply patches for solarized color scheme and custom font/fontsize
+wget https://st.suckless.org/patches/solarized/st-no_bold_colors-20170623-b331da5.diff
+wget https://st.suckless.org/patches/solarized/st-solarized-both-20170626-b331da5.diff
+patch < "$HOME"/.files/setup/st-custom-font-20170928-b331da5.diff
+patch < st-no_bold_colors-20170623-b331da5.diff
+patch < st-solarized-both-20170626-b331da5.diff
+
+sudo make clean install

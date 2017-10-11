bec52a2fe075766ea1f094fd250fe05d7e7f3929
pylipp <philipp.metzner@tum.de>
2017-10-12 00:34:52 +0200

    Update qutebrowser installation script.

--- install_qutebrowser.bash
+++ install_qutebrowser.bash
@@ -4,13 +4,16 @@ source "$HOME"/.files/setup/utils.bash
 
 mkcswdir
 
-install_packages python3-lxml python3-pyqt5 \
-    python3-pyqt5.qtwebkit python3-pyqt5.qtquick python3-sip python3-jinja2 \
-    python3-pygments python3-yaml python3-pyqt5.qtsql libqt5sql5-sqlite \
-    python3-pyqt5.qtwebengine python3-pyqt5.qtopengl
-
+install_packages python3-pyqt5 python3-pyqt5.qtwebkit python3-pyqt5.qtquick python-tox \
+    python3-sip python3-dev python3-pyqt5.qtsql libqt5sql5-sqlite
 git clone https://github.com/qutebrowser/qutebrowser
-cd qutebrowser
 
-pip3 install --user .
-# QT_XCB_FORCE_SOFTWARE_OPENGL=1 qutebrowser --backend webengine index.html
+cd qutebrowser
+tox -v -e mkvenv-pypi
+cd ..
+
+# generate docs
+install_packages asciidoc source-highlight
+git clone https://github.com/asciidoc/asciidoc
+cd qutebrowser/scripts
+./asciidoc2html.py --asciidoc python2 ~/software/asciidoc/asciidoc.py

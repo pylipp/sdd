77cff27771b82ada5cd34f523c48efb9c7f9308d
pylipp <philipp.metzner@tum.de>
2017-10-16 17:25:04 +0200

    Add info on how to make qutebrowser the default browser.

--- install_qutebrowser.bash
+++ install_qutebrowser.bash
@@ -20,3 +20,13 @@ cd qutebrowser/scripts
 
 # TODO: set links in individual installation files or in single script?
 setup_xdg_config_link qutebrowser qutebrowser_config.py config.py
+
+# make x-www-browser call qutebrowser
+sudo update-alternatives --install /usr/bin/x-www-browser x-www-browser $HOME/.files/bin/qutebrowser 200
+update-alternatives --config x-www-browser
+
+# to make thunderbird open links in qutebrowser, edit ~/.thunderbird/<profile>.default/prefs.js
+# the network.protocol-handler.warn-external.* entries require the value 'true'
+# also delete ~/.thunderbird/<profile>.default/mimeTypes.rdf
+# then thunderbird asks for a new default application to open links etc.
+# see also: http://kb.mozillazine.org/Setting_Your_Default_Browser#Setting_the_browser_that_opens_in_Thunderbird_-_Linux

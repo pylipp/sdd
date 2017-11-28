65fd60940351eb29a6ff8afc064f8598f09bf483
pylipp <philipp.metzner@tum.de>
2017-11-28 13:06:39 +0100

    Tmux installation script.

---
+++ setup_tmux.bash
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+# Installation script for tmux and plugins
+# Installation location: /usr and ~/.tmux
+# Requires: - git
+#           - pip
+#           - make/cmake
+
+set -e
+
+source ~/.files/setup/utils.bash
+
+
+main() {
+    local method=${1:-global}
+
+    echo_install_info tmux
+
+    if [[ $method = "global" ]]; then
+        install_packages tmux xclip build-essential cmake
+    elif [[ $method != "local" ]]; then
+        echo_error "Unknown method '$method'!"
+        exit 1
+    fi
+
+    [[ $(command -v tmux >/dev/null 2>&1) ]] || ( echo_error "Tmux not installed!"; exit 1; )
+
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    pip install --user tmuxp
+
+    # set up config files for plugin installation
+    mv_existing ~/.tmux.conf
+    mv_existing ~/.tmuxp
+    ln -s ~/.files/tmux.conf ~/.tmux.conf
+    ln -s ~/.files/tmuxp ~/.tmuxp
+
+    cd ~/.tmux/plugins
+    tpm/bin/install_plugins
+
+    # check out custom branch
+    cd tmux-mem-cpu-load
+    git checkout feature/temperature
+    # issue warning; tmux-mem-cpu-load pluging requires cmake
+    [[ $(command -v cmake >/dev/null 2>&1) ]] || echo_error "cmake not installed!"
+}
+
+main "$@"

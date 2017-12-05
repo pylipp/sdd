9efecd70c0db9bf08c0cae7bd2e1e1dc12081504
pylipp <philipp.metzner@tum.de>
2017-12-05 15:43:04 +0100

    Add local xkb installation.

--- complete_setup.bash
+++ complete_setup.bash
@@ -8,6 +8,17 @@ if [[ $method = "global" ]]; then
     sudo apt-get update && sudo apt-get upgrade -y
     sudo cp "$HOME/.files/keyboard" /usr/share/X11/xkb/symbols/pylipp
     setxkbmap pylipp
+elif [[ $method = "local" ]]; then
+    mkdir -p ~/.config/xkb/symbols
+    ln -s ~/.files/keyboard ~/.config/xkb/symbols/pylipp
+    cd ~/.config/xkb
+    # worked: http://www.gabriel.urdhr.fr/2014/06/06/custom-keyboard/
+    # did not work: https://unix.stackexchange.com/questions/397716/custom-keyboard-layout-without-root?rq=1
+    setxkbmap pylipp pylipp -print | xkbcomp -I. - $DISPLAY
+    cd -
+else
+    echo_error "Unknown method '$method'!"
+    exit 1
 fi
 
 for step in install_core_utils setup_zsh setup_python install_vim setup_tmux install_st; do

3812fcf22a91fdf5ab294d85930bfd25a54b61e8
pylipp <philipp.metzner@tum.de>
2017-12-11 13:46:10 +0100

    Simpler screen lock procedure using i3lock and minimal script.

--- setup_i3.bash
+++ setup_i3.bash
@@ -16,7 +16,7 @@ main() {
         echo_install_info i3
 
         install_packages git build-essential
-        install_packages i3 xautolock xorg xinit feh imagemagick xrandr arandr
+        install_packages i3 xautolock xorg xinit feh scrot imagemagick arandr
 
         mkcswdir
 
@@ -33,12 +33,12 @@ main() {
         install_packages pkg-config libxcb1 libpam-dev libcairo-dev \
             libxcb-xinerama0-dev libev-dev libx11-dev libx11-xcb-dev libxkbcommon0 \
             libxkbcommon-x11-0 libxcb-dpms0-dev libxcb-image0-dev libxcb-util0-dev \
-            libxcb-xkb-dev libxkbcommon-x11-dev libxkbcommon-dev dbus-x11
-        git clone https://github.com/chrjguill/i3lock-color
-        cd i3lock-color 
+            libxcb-xkb-dev libxkbcommon-x11-dev libxkbcommon-dev dbus-x11 \
+            libxcb-randr0 libxcb-randr0-dev
+        git clone https://github.com/i3/i3lock
+        cd i3lock
+        git checkout 2.10  # does not require autotools
         sudo make install 
-        cd ~/.files/i3/i3lock-fancy
-        git checkout multimonitor
     elif [[ $method = "local" ]]; then
         echo_warn "Sorry, local i3 installation is not possible."
     else

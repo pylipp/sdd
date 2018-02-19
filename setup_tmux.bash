7cbc7018b94d7e4650367d0832906594dc3bbdea
pylipp <philipp.metzner@tum.de>
2018-02-19 08:54:40 +0100

    Use tpm fork with plugin branch installation feature

--- setup_tmux.bash
+++ setup_tmux.bash
@@ -33,7 +33,7 @@ main() {
 
     [[ $(command -v tmux >/dev/null 2>&1) ]] || ( echo_error "Tmux not installed!"; exit 1; )
 
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/ashneo76/tpm --branch customizations ~/.tmux/plugins/tpm
     pip install --user tmuxp
 
     # set up config files for plugin installation
@@ -45,9 +45,6 @@ main() {
     cd ~/.tmux/plugins
     tpm/bin/install_plugins
 
-    # check out custom branch
-    cd tmux-mem-cpu-load
-    git checkout feature/temperature
     # issue warning; tmux-mem-cpu-load pluging requires cmake
     [[ $(command -v cmake >/dev/null 2>&1) ]] || echo_error "cmake not installed!"
 }

34e2cd0cf117d1b8bb1fbe9528d6aa70fb5c2480
pylipp <philipp.metzner@tum.de>
2019-03-25 21:43:34 +0100

    Update watson installation script
    - exit with 1 when given invalid command
    - enable pip installation with any git revision
    - create directory for zsh functions if not existing
    - download raw file from github
    - remove wget -N option (no effect in combination with -O)

--- install_watson.bash
+++ install_watson.bash
@@ -2,27 +2,34 @@
 
 # Installation script for watson
 # Installation location: $WORKON_HOME/watson
-# Requires: - python-virtualenv
+# Requires: - python3-virtualenv
 #           - wget
+#           - git
 
 set -e
 
-WATSON_VERSION=1.6.0
+WATSON_VERSION=aa901567c5aa6129ff6dae799eddbfb0be06cb65
 
 main() {
     if [[ $1 = "install" ]]; then
-        python -m virtualenv --python=$(which python3) $WORKON_HOME/watson
+        python3 -m virtualenv --python=$(which python3) $WORKON_HOME/watson
         source $WORKON_HOME/watson/bin/activate
-        pip install td-watson==$WATSON_VERSION
+        pip install git+https://github.com/TailorDev/Watson@$WATSON_VERSION\#egg=Watson
+
+        mkdir -p ~/.local/bin
         ln -s $WORKON_HOME/watson/bin/watson ~/.local/bin
-        wget -O ~/.local/share/zsh/site-functions/_watson https://github.com/TailorDev/Watson/blob/$WATSON_VERSION/watson.zsh-completion
+
+        mkdir -p ~/.local/share/zsh/site-functions
+        wget -O ~/.local/share/zsh/site-functions/_watson https://raw.githubusercontent.com/TailorDev/Watson/$WATSON_VERSION/watson.zsh-completion
+
         mkdir -p ~/.config/watson
         ln -s ~/.files/watson_config ~/.config/watson/config
 
     elif [[ "$1" = "update" ]]; then
         source $WORKON_HOME/watson/bin/activate
-        pip install -U td-watson==$WATSON_VERSION
-        wget -N -O ~/.local/share/zsh/site-functions/_watson https://github.com/TailorDev/Watson/blob/$WATSON_VERSION/watson.zsh-completion
+        pip install --upgrade git+https://github.com/TailorDev/Watson@$WATSON_VERSION\#egg=Watson
+
+        wget -O ~/.local/share/zsh/site-functions/_watson https://raw.githubusercontent.com/TailorDev/Watson/$WATSON_VERSION/watson.zsh-completion
 
     elif [[ "$1" = "remove" ]]; then
         rm -rfv ~/.local/bin/watson $WORKON_HOME/watson ~/.local/share/zsh/site-functions/_watson ~/.config/watson/config
@@ -32,6 +39,7 @@ main() {
     else
         echo "No or incorrect command given: $*"
         echo "Select one of: install | update | remove"
+        exit 1
     fi
 }
 

f8ff926b0150c53209aa13ec61ec9ab223ee14bf
pylipp <philipp.metzner@tum.de>
2019-04-07 21:47:07 +0200

    Enable skipping of app tests

--- test/run.sh
+++ test/run.sh
@@ -4,8 +4,16 @@
 
 # Usage: ./run.sh [--debug]
 # Options:
-#   --debug     Leave container alive after running the suite
+#   --debug         Leave container alive after running the suite
+# Environment variables:
+#   NO_APP_TESTS    If non-empty, run only framework tests
 
 container_id=$(docker run -d -it --rm --volume "$(pwd)":/opt/sdd --workdir /opt/sdd/test sdd:latest)
-docker exec $container_id bats -r .
+
+if [[ -z $NO_APP_TESTS ]]; then
+    docker exec $container_id bats -r .
+else
+    docker exec $container_id bats -r framework
+fi
+
 [ "$1" != "--debug" ] && docker kill $container_id

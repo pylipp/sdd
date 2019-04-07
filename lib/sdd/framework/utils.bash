c07313bca00d8c30cc3edc7542b4dc03dac9e28f
pylipp <philipp.metzner@tum.de>
2019-04-07 11:40:22 +0200

    Extend install command functionality

--- lib/sdd/framework/utils.bash
+++ lib/sdd/framework/utils.bash
@@ -1,11 +1,29 @@
 # Utility functions for sdd framework
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 utils_install() {
     # Install one or more apps
     if [ $# -eq 0 ]; then
         printf 'Specify at least one app to install.' >&2
         return 1
     fi
+
+    # Iterate over arguments
+    local appfilepath
+    for app in "$@"; do
+        appfilepath="$SCRIPTDIR/../apps/user/$app"
+
+        # Check whether filepath exists
+        if [ ! -f "$appfilepath" ]; then
+            printf 'App "%s" could not be found.' "$app" >&2
+            return 2
+        else
+            # Source app management file and execute installation function if found
+            source "$appfilepath"
+            sdd_install 2>/dev/null || { printf 'No sdd_install function for "%s" provided' "$app" >&2; return 4; }
+        fi
+    done
 
     return 0
 }
--- test/framework/bin.bats
+++ test/framework/bin.bats
@@ -8,3 +8,21 @@
   [ "$status" -eq 1 ]
   [ "$output" = 'Specify at least one app to install.' ]
 }
+
+@test "invoking install command with invalid app fails" {
+  run sdd install invalid_app
+  [ "$status" -eq 2 ]
+  [ "$output" = 'App "invalid_app" could not be found.' ]
+}
+
+@test "invoking install command with valid app but without sdd_install present fails" {
+  local appfilepath=../lib/sdd/apps/user/valid_app
+  touch $appfilepath
+
+  run sdd install valid_app
+  [ "$status" -eq 4 ]
+  [ "$output" = 'No sdd_install function for "valid_app" provided' ]
+
+  rm $appfilepath
+  [ ! -f $appfilepath ]
+}

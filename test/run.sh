6233a61c28f27353f528b9e94209badb3be2d39f
pylipp <philipp.metzner@tum.de>
2019-04-07 11:50:22 +0200

    Kill container in case of failing test

--- test/run.sh
+++ test/run.sh
@@ -2,8 +2,6 @@
 
 # Test runner for sdd test suites
 
-set -e
-
 container_id=$(docker run -d -it --rm --volume "$(pwd)":/opt/sdd --workdir /opt/sdd/test sdd:latest)
 docker exec $container_id bats -r .
 docker kill $container_id

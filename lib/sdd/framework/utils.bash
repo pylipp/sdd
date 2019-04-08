75f246fed82fce739f30f92d9c324dc64427387b
pylipp <philipp.metzner@tum.de>
2019-04-08 21:25:10 +0200

    End printf commands with newline

--- lib/sdd/framework/utils.bash
+++ lib/sdd/framework/utils.bash
@@ -5,7 +5,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 utils_install() {
     # Install one or more apps
     if [ $# -eq 0 ]; then
-        printf 'Specify at least one app to install.' >&2
+        printf 'Specify at least one app to install.\n' >&2
         return 1
     fi
 
@@ -16,7 +16,7 @@ utils_install() {
 
         # Check whether filepath exists
         if [ ! -f "$appfilepath" ]; then
-            printf 'App "%s" could not be found.' "$app" >&2
+            printf 'App "%s" could not be found.\n' "$app" >&2
             return 2
         else
             # Source app management file and execute installation function if found
@@ -25,9 +25,9 @@ utils_install() {
             sdd_install 2>$stderrlog
 
             if [ $? -eq 0 ]; then
-                printf 'Installed "%s".' "$app"
+                printf 'Installed "%s".\n' "$app"
             else
-                printf 'Error installing "%s": %s' "$app" "$(<$stderrlog)" >&2
+                printf 'Error installing "%s": %s\n' "$app" "$(<$stderrlog)" >&2
                 return 4
             fi
         fi

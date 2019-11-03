6fbf4fd2fe3373b0b276cf43ee0d3b1331c267e5
pylipp <philipp.metzner@tum.de>
2019-11-03 22:14:40 +0100

    Add option to merely attach to container instead of running tests

--- test/run.sh
+++ test/run.sh
@@ -2,13 +2,20 @@
 
 # Test runner for sdd test suites
 
-# Usage: ./run.sh [--debug]
+# Usage: ./run.sh [--debug] [--open]
 # Options:
 #   --debug         Attach to container after running tests
+#   --open          Attach to container instead of running tests
 # Environment variables:
 #   NO_APP_TESTS    If non-empty, run only framework tests
 
 container_id=$(docker run -d -it --rm --volume "$(pwd)":/opt/sdd --workdir /opt/sdd/test pylipp/sdd:latest)
+
+if [ "$1" = "--open" ]; then
+    docker attach $container_id
+    exit
+fi
+
 
 if [[ -z $NO_APP_TESTS ]]; then
     docker exec $container_id bats -r .
